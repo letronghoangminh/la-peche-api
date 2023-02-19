59745273a44fcf7ced775d5697e16286e9d31c4d
letronghoangminh <letronghoangminh@gmail.com>
2023-02-19 14:55:22 +0700

    feat: add friend relationship to User model

--- prisma/migrations/20230219075444_init_db/migration.sql
+++ prisma/migrations/20230219075444_init_db/migration.sql
@@ -118,6 +118,15 @@ CREATE TABLE `_user_stars` (
     INDEX `_user_stars_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `_user_friends` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_user_friends_AB_unique`(`A`, `B`),
+    INDEX `_user_friends_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `notification` ADD CONSTRAINT `notification_userId_fk` FOREIGN KEY (`id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
@@ -144,3 +153,9 @@ ALTER TABLE `_user_stars` ADD CONSTRAINT `_user_stars_A_fkey` FOREIGN KEY (`A`) 
 
 -- AddForeignKey
 ALTER TABLE `_user_stars` ADD CONSTRAINT `_user_stars_B_fkey` FOREIGN KEY (`B`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_user_friends` ADD CONSTRAINT `_user_friends_A_fkey` FOREIGN KEY (`A`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_user_friends` ADD CONSTRAINT `_user_friends_B_fkey` FOREIGN KEY (`B`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
