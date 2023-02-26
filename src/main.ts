856f8591c26a198b43b402895e40b06668f2fe8a
letronghoangminh <letronghoangminh@gmail.com>
2023-02-26 15:00:12 +0700

    [LPSD-40] fix: change port for api

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v1');
+  app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
 
   const swaggerConfig = new DocumentBuilder()
