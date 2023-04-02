eeb9c68ea29164885ff51f046932b055b77318ac
letronghoangminh <letronghoangminh@gmail.com>
2023-04-02 15:55:44 +0700

    [LPSD-90] feat: add validation for password

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -75,7 +75,13 @@ export class ResetPasswordDto {
   @ApiProperty({ type: String, required: true, nullable: false })
   token: string;
 
-  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ type: String, required: true, nullable: false })
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   password: string;
 }
